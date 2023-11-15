07a1d24b20c5f8ebe65441a4ad480d17b4c6a098
Jinwoo Park <skin_lotion@naver.com>
2023-11-15 19:53:25 +0900

    godlife-project-auth-dto

--- src/main/java/com/godlife/godlifeback/dto/request/auth/SignUpRequestDto.java
+++ src/main/java/com/godlife/godlifeback/dto/request/auth/SignUpRequestDto.java
@@ -1,9 +1,7 @@
 package com.godlife.godlifeback.dto.request.auth;
 
-import javax.validation.constraints.AssertTrue;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -16,23 +14,16 @@ import lombok.Setter;
 @NoArgsConstructor
 public class SignUpRequestDto {
 
-    @NotBlank @Email
+    @NotBlank @Email @Pattern(regexp = "/^[a-zA-Z0-9_]*@([-.]?[a-zA-Z0-9])*\\.[a-zA-Z]{2,4}$/")
     private String email;
 
-    @NotBlank @Size(min=8, max=20)
+    @NotBlank @Size(min=8, max=20) @Pattern(regexp= "/^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_?]).{8,15}$/")
     private String password;
 
     @NotBlank
     private String nickname;
 
-    @NotBlank @Pattern(regexp="^[0-9]{11,13}$")
-    private String telNumber;
-
     @NotBlank
-    private String address;
+    private String Category;
 
-    private String addressDetail;
-
-    @NotNull @AssertTrue
-    private Boolean agreedPersonal; 
 }
