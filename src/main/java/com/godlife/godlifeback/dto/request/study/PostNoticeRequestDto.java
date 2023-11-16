8ea13fb5e6d639ce08cc1a661e71971f02e3e243
khkim <stakkh4@gmail.com>
2023-11-16 12:01:36 +0900

    notice entity request responst 생성

---
+++ src/main/java/com/godlife/godlifeback/dto/request/study/PostNoticeRequestDto.java
@@ -0,0 +1,11 @@
+package com.godlife.godlifeback.dto.request.study;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PostNoticeRequestDto{
+    private String noticeContent;
+}
---
+++ src/main/java/com/godlife/godlifeback/dto/response/study/PostNoticeResponseDto.java
@@ -0,0 +1,33 @@
+package com.godlife.godlifeback.dto.response.study;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.godlife.godlifeback.dto.response.ResponseCode;
+import com.godlife.godlifeback.dto.response.ResponseDto;
+import com.godlife.godlifeback.dto.response.ResponseMessage;
+import com.godlife.godlifeback.entity.StudyNoticeEntity;
+
+import lombok.Getter;
+
+@Getter
+public class PostNoticeResponseDto extends ResponseDto{
+    private int studyNoticeNumber;
+    private int studyNumber;
+    private String noticeContent;
+
+    private PostNoticeResponseDto(String code, String message, StudyNoticeEntity studyNoticeEntity ){
+        super(code, message);
+
+        this.studyNoticeNumber = studyNoticeEntity.getNoticeNumber();
+        this.studyNumber = studyNoticeEntity.getStudyNumber();
+        this.noticeContent = studyNoticeEntity.getNoticeContent();
+    }
+
+    public static ResponseEntity<PostNoticeResponseDto> success(StudyNoticeEntity studyNoticeEntity){
+        PostNoticeResponseDto  result = new PostNoticeResponseDto(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, studyNoticeEntity);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+}
