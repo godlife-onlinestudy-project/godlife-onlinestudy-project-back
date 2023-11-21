63c4f4e4b701f73f5b9496028b8f3e3f9bada980
khkim <stakkh4@gmail.com>
2023-11-21 09:52:42 +0900

    공지사항: 리스트 아이템, patch reqeust Dto, post requestDto,get response Dto ,post response Dto,  delete response Dto, patch response Dto, 엔터티, 레포지토리, 유저 리스트 : 리스트 아이템 생성

---
+++ src/main/java/com/godlife/godlifeback/common/object/StudyUserItem.java
@@ -0,0 +1,10 @@
+package com.godlife.godlifeback.common.object;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class StudyUserItem {
+    
+}
---
+++ src/main/java/com/godlife/godlifeback/repository/StudyNoticeRepository.java
@@ -0,0 +1,16 @@
+package com.godlife.godlifeback.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.godlife.godlifeback.entity.StudyNoticeListEntity;
+import java.util.List;
+
+@Repository
+public interface StudyNoticeRepository extends JpaRepository<StudyNoticeListEntity,Integer>{
+    
+    StudyNoticeListEntity findByNoticeNumber(Integer studyNoticeNumber);
+
+    List<StudyNoticeListEntity> findByStudyNoticeNumber(Integer studyNumber);
+    
+}
